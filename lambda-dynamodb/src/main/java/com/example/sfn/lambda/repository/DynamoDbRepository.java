cf35bc690e72262c04c9a0844ab9987435e76139
Igor_almeida <igoor_sp@hotmail.com>
2025-02-26 16:10:45 -0300

    refactory constants

--- lambda-dynamodb/src/main/java/com/example/sfn/lambda/repository/DynamoDbRepository.java
+++ lambda-dynamodb/src/main/java/com/example/sfn/lambda/repository/DynamoDbRepository.java
@@ -15,6 +15,11 @@ public class DynamoDbRepository {
 
     private final DynamoDbClient dynamoDbClient;
 
+    private static final String STATUS = "status";
+    private static final String EXECUTION_ID = "executionId";
+    private static final String BUSINESS_KEY = "businessKey";
+    private static final String EXECUTION_START_TIME = "executionStartTime";
+    private static final String TASK_TOKEN = "taskToken";
     @Inject
     public DynamoDbRepository(DynamoDbClient dynamoDbClient) {
         this.dynamoDbClient = dynamoDbClient;
@@ -24,11 +29,11 @@ public class DynamoDbRepository {
                             final String tableName) {
         Map<String, AttributeValue> item = new HashMap<>();
 
-        item.put("businessKey", AttributeValue.builder().s(message.getBusinessKey()).build());
-        item.put("executionStartTime", AttributeValue.builder().s(message.getExecutionStartTime()).build());
-        item.put("executionId", AttributeValue.builder().s(message.getExecutionId()).build());
-        item.put("taskToken", AttributeValue.builder().s(message.getTaskToken()).build());
-        item.put("status", AttributeValue.builder().s("PENDING").build());
+        item.put(BUSINESS_KEY, AttributeValue.builder().s(message.getBusinessKey()).build());
+        item.put(EXECUTION_START_TIME, AttributeValue.builder().s(message.getExecutionStartTime()).build());
+        item.put(EXECUTION_ID, AttributeValue.builder().s(message.getExecutionId()).build());
+        item.put(TASK_TOKEN, AttributeValue.builder().s(message.getTaskToken()).build());
+        item.put(STATUS, AttributeValue.builder().s("PENDING").build());
 
         PutItemRequest putItemRequest = PutItemRequest.builder()
                 .tableName(tableName)
